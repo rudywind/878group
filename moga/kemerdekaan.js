82ce3446bee02d38a623b81a19045c886437143e
rudywind <35211883+rudywind@users.noreply.github.com>
2025-08-22 20:32:02 +0700

    Create kemerdekaan.js

---
+++ moga/kemerdekaan.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      a.smartb2 {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj53qRoH1oC9xwoFMazxDYJC5YWP-BrLbyIf8_4Aqx9aMlna8Aszp1o1k3Rp7Ve5oAhA06lmkYZTMbzIl717mybbydRgxuRJjK5DW0yNVfgvkBx9S8KgIezLIyxu6U0t7ZBpjakZbL_xEUwlmnu90IQZ5MtmBrKtDqwgDJjbV0zGPejDcgr7n7uVWkno0o/s1600/navbar-mobile-moga.png) !important;
+          background-position: center !important;
+          background-size: cover !important;
+          box-shadow: 0 5px 10px #0000007d;
+      }
+      .main, .main-content {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjRi62nfjfuMVkDM-oCIp_ljwW7WorMubZJoNIT8ijYU671I192glCjpPPf9uJkJreb9Cb_7ZYso8AlL_yDYm73PDax5VOwspYamY4I5EpVu0EvCIOh0m2pWJDDuRjj2sW-ko0eOevnZrR2V-wbzjNCvfSi3wz3HxUTk-UxmnTQy4J2LTsfQwhVf5UaqKs/s1600/MOBILE-BACKGROUND-MOGA%20%281%29.jpg) !important;
+          background-size: 100% !important;
+      }
+      .navbar-fixed-top {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjst5k3zBbMu6ScBvniyrIb0gH2zOEjYTa1eNT6mMfcnl-hViq1e6OODBfKXXOQmeKXGET9egiL7I-eH5dIMV-aE8n3k9uCCXfzZZpeqMKWcCvZaNUEmlCuIOyDRf1jP_YpZnTRk5WW_IpLXKAYjIQwmeoTf3MEBnTakfiNy7pl63-5gQSGFRohM-_vyA/s1600/navbar-moga17-agustus.png);
+          background-size: cover;
+          background-repeat: no-repeat;
+          box-shadow: none !important;
+      }
+      body {
+          background: url(https://i.ibb.co/bjc6ryJK/moga-17-agustus-backgrond.jpg) !important;
+          background-attachment: fixed !important;
+          background-size: 100% !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
