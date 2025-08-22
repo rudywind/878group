e83f88cfa30a0f5dcfeb7b4f342536c2e9d157bb
rudywind <35211883+rudywind@users.noreply.github.com>
2025-08-22 15:54:17 +0700

    Create tema-default.js

---
+++ bisajp/tema-default.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDefault = `
+    <style>
+      body {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgvYGlI-LDUhC9yHt23B3mnkR9mm6cxkHySpbr7St8BXA9Rbv-BwoAE-eXiC25lxkJXYcbtKv2lu7JyULsZBJTDgnbb2C2Px5mdIQKpP9J1Hnxx9WUeH5XZcDnQeSzyx2UG9v57rcIGPs2pwihkcxIxxC6f41ES2WUn59yusldVHbg7P9raZNSX1FG-Rqg/s1600/jp-bakcground-default.jp) !important;
+          background-attachment: fixed !important;
+          background-size: cover !important;
+      }
+      
+      .navbar-fixed-top {
+          background-color: transparent !important;
+          box-shadow: none !important;
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi7n9Sqxkl3Qkjpuib_NCTOzN_aH0LSEjLilENvRtxxhyphenhyphenJRZVmbDQtoTYcEIslQlWNZx5ToUppX8Io-j9p3tnf2rkf3esUcUf-k_G2Zn7eKeV67Zd01gd3QNabgi7Z6IHUiflAhVZjFNmWDkaw1ItA7GodjHS2lFPZ_vXskPTP4QF8YfdekG2kkN4VyXx0/s1600/navbar-deafult-jp.jpg) bottom center no-repeat;
+          background-size: cover;
+      }
+      
+      .main-content, 
+      .content, 
+      .no-bottom{
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMrKd6l-KEJGw9bNPa0jbwmMmqNQyxFShL3q0HDVE2ITFtRDD2v0NmgnW4A_KA-eA3PfBUFzQL6xsyjGdEptbXYsauZW_Hy10udjJu8mfnnmVrKIQqmRw9Gx-YOO3_WULf2Fy1BhTxreUxkoH83NieAaopV9TPyt66-9-aGLHfOaz0Qy0lGhGvuz3iI2c/s1600/MOBILE-BACKGROUND-default.jpg) !important;
+          background-size: 100% !important;
+      }
+      
+      .smartb2 .header-logo2 {
+          width: 90px !important;
+          height: 69px !important;
+          margin: 0 auto !important;
+          float: none !important;
+      }
+      
+      a.smartb2 {
+          background: #000000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg81H9Peo4kitQC14TFk5p8smkNWsAp7_ccAzrTq1hqjDJk5AvI28122WBVdZa8_MS6i1xIdFkaZRNPSHv_lfD2AN27jSHAhcgtgCzIb4sEDEWxSgVES54z1qObjX-H0FCXtlXQ7OxwIsEwLU41IgaMCgzkgl9kY4xWIo851s9iwMDIktSEZCoTgT6yHww/s1600/238c5ab….png) !important;
+          background-position: center !important;
+          background-size: 100% !important;
+          box-shadow: 0 5px 10px #0000007d;
+      }
+    </style>`;
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDefault);
+});
