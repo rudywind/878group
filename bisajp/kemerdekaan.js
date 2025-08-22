16ababe66d5f103d213ae5c6f24f8a8758ad91de
rudywind <35211883+rudywind@users.noreply.github.com>
2025-08-22 15:56:15 +0700

    Create kemerdekaan.js

---
+++ bisajp/kemerdekaan.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      a.smartb2 {
+          background: #000000 url(https://i.ibb.co/60R9HYBP/navbar-mobile-jp.png) !important;
+          background-position: center !important;
+          background-size: cover !important;
+          box-shadow: 0 5px 10px #0000007d;
+      }
+      .main-content, .content, .no-bottom {
+          background: url(https://i.ibb.co/VY5tyJ1k/background-mobile-17-agustus.jpg) !important;
+          background-size: 100% !important;
+      }
+      body {
+          background: url(https://i.imgur.com/aPBLvHg.jpeg) !important;
+          background-attachment: fixed !important;
+          background-size: 100% !important;
+      }
+      body {
+          background: url(https://i.imgur.com/aPBLvHg.jpeg) !important;
+          background-attachment: fixed !important;
+          background-size: 100% !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
