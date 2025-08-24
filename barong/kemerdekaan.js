e7550712fb183b4c09431b7fb59a39b6b7beb648
rudywind <35211883+rudywind@users.noreply.github.com>
2025-08-24 18:31:53 +0700

    Create kemerdekaan.js

---
+++ barong/kemerdekaan.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      body {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh9SMy-EzLVxkjp-qdVz3-XZYWKhiFUqzfCUzifotksIj_4HDB-vRriQmI-hhTvbxeYAzxbq8IH9NV4oBF4Mc0GaaXLy40TmGsekODXMzci_L9Sc3eg6mM4rj1DXJA07iSg9MSX_7I3PLiM0iiC3rFILHdjx4yewfuVzG3WbCWveFrLmr3qOZxxVdXOX34/s16000/barong-17-agustus.jpg) !important;
+          background-attachment: fixed !important;
+          background-size: 100% !important;
+      }
+      .navbar-fixed-top {
+          background-color: transparent !important;
+          box-shadow: none !important;
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjohbqbUsBZnzAorhdX8Xj9n6a-MRQZZLfx84JmBZRxbitMNKXhanTqbAy0Fy7aTagXK7i3puqarWfL4k6eqGnrdyuV7H9OkndZ-Q-oSaqn-wvYaycE4oKRp8iPofPDx1KZD6GwM0FkWyX5yEgWtwgu60hb3rQVQ7RKPPFFw2SadrdQJBE3IEcv67zcC6c/s16000/navbar-barong-17-agustus.png) bottom center no-repeat;
+          background-size: cover;
+      }
+      #content .page-header a.smartb2 {
+          background: #610000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgHKGVpHkASYeza7Vtgt32hOBqFJcvu8tFBLvFv-aR24WEU_qJTXYPo25rstib96Z_L7_sC_JCeGMgo0nBjN7RLdSZVn-UL9avlN_pKFEKIQGSIJbILyDHG0qTQo5-gVNQ1zUU7xRhwbD25C5zqqFLohXF0Goi61l_dBnx_5FPdpKnWr-oDfjFmv0aLrzM/s1600/17-agustus-navbar-mobile.png) !important;
+          background-position: center !important;
+          background-size: cover !important;
+          box-shadow: 0 5px 10px #0000007d;
+      }
+      .smartb2 .header-logo2 {
+          width: 200px !important;
+          height: 65px !important;
+          margin: 0 auto !important;
+          float: none !important;
+          margin-top: 5px !important;
+          display: block;
+      }
+      .main, .main-content {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhSitPC3j6EGabwZ_TMZow0akNBL4_Ma7n1lT0QJwizEnyIslgnzAOYkYHo2Hlh0KApYj0HFLJV_sEb2lsz17gpXo-i8RDzH8By6XIr7pkgzLPhKyXDV5XhsxWAlz1nhX5Zrl5IEtAv4wnpGh5Vx3AIeo11EMHSe_LL479xvU60qc5dwdxjBYC5t4lXsWeZ/s4000/mobile-bg.jpg) !important;
+          background-size: 100% !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
+
