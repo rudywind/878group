8b42cebb94258d8908f9198381ae4d28a307e674
rudywind <35211883+rudywind@users.noreply.github.com>
2025-08-24 18:37:09 +0700

    Create default.js

---
+++ barong/default.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      body {
+      background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiiN1UATHCO2deqbBzI001s7HOVhHbHFtDeFBuBBV_nHMpLrA6pOKq6d8wPSoTqDwOVOUKKMHafwUcVfBa6ha7blX1ezc9hWU__ok0GH56UvWwhG7CXRpVejgCyyFsOi03lGzt3sZS00sxz5YglTwr2-sF-PtK8xUgncnMBNpOgAf03So40YrXrMyeXbeM/s1600/1-barong.jpg) !important;
+      background-attachment: fixed !important;
+      background-size: 100% !important;
+  }
+      #content .page-header a.smartb2 {
+        background: #610000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiAor6lJcdS6SkJTWST38JpTEV0ufewvU2dO-7Bp65rBXKdOrlbm-FxxmEZum3y2PeaHCoJSAv6EYMpWJYjUnwnQV0I57ijC-C9daeZbwltFhGP_-FobF8SgLleRnMAokLynvGnxz_QOjh57BW7iHMMQi0eChF4-_jva-34HWJNFAZMJQc6zqBkMZT3yKI/s1600/nav.jpg) !important;
+        background-position: center !important;
+        background-size: cover !important;
+        box-shadow: 0 5px 10px #0000007d;
+    }
+    .smartb2 .header-logo2 {
+        width: 161px !important;
+        height: 55px !important;
+        margin: 0 auto !important;
+        float: none !important;
+        margin-top: 20px !important;
+        display: block;
+    }
+    .main, .main-content {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhEvROLQtGn4EjDJ7Tzp31WuiFhzcwGMBPrnTPCmzEkfMl1hr5uS9BbO25FbYrMAzLcoEZkYQxsJRW48gque07WKIxnFedduHThbtUNbnB-8Q3LMfrR__tEmayUnG1_28L97No0nkmh2B1iAnmvl5BuwNS67fdMn28VeTh-BaGhK51derUBiQiIXo6MhME/s1600/MOBILE-BACKGROUND.jpg) !important;
+        background-size: 100% !important;
+    }
+    .navbar-fixed-top {
+        background-color: transparent !important;
+        box-shadow: none !important;
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhbh2Q2ma9YHoCHjITG3_4bs-36F3yha0aUns5ddth4AoXeW7LSUAgspSvhBm51O1nS_JV2VXo9YZZj6V3NjqDxH59nuB_ScY6a-I0ad_5VovEOSXsvprJ76vJwGLFpbKUea8QLUIVL2AnaNp3fPtv79JKZSpcX3QbBHnG_dUd_e2FSmD0yayH9Nerq48I/s1600/navbar-barong.png) bottom center no-repeat;
+        background-size: cover;
+    }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
+
