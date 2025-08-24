fb6b719eb0a6aec96bd00e496e72c8029a54b137
rudywind <35211883+rudywind@users.noreply.github.com>
2025-08-24 13:57:34 +0700

    Create default.js

---
+++ moga/default.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      body {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh-C0EbcnHcZ2jIosFOYwF0TArYe0o9q1mRSMWMtD3dr23kxz4_U1A39_68MsEH2cgb8KkUUjcDmyf-zD-jXEgsT2KLa5oNj4MfESb2JXxJLTJjrO9J1vycevKQfwFzuJiL8Vq2dq7DFUUQK0rMr6muiUP4XQwuvfKBqQDiaEBqDmsPWnbU3wGE3hC4Q0w/s1600/70001952_moso-16.jpg) !important;
+        background-attachment: fixed !important;
+        background-size: 100% !important;
+      }
+      .navbar-fixed-top {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQ3jrMbzAUA7hO9992yY7rY7opIEA89f62LwqwomLcq0-waW0PCo0orO68L3UyEaUq-xVIszlqHadHu5_eXoYBHw6BJYiVMIe5EqZHMhPmR9PbhFdiINS6wE91JO68xWlEhX9Pjb2BC0HiUWa8AaJNcmtKHyputUOG5LsayjquOF5ooxnpe4rZodEEB4E/s1600/navbar-biasa.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        box-shadow: none !important;
+      }
+      a.smartb2 {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhYU1KS42uJ4M-KiSb0VHpcLREG_KEUlvp3crZxRlEqXnVBkouGpEO4Q_Y7CHpgr2zOJi3wwYy6qf-LidTc_8mgpzenDPnZE-G2zGMlqcChUQEG7P7iFymhWqJUiB4Lu-D6qy0Bt3FDjC-e37mz1hHmJK6ioTF2IM8DbISnB8c43aT2TUBp8yIKnGh0xlU/s1600/238c5ab….jpg) !important;
+        background-position: center !important;
+        background-size: cover !important;
+        box-shadow: 0 5px 10px #0000007d;
+      }
+      .smartb2 .header-logo2 {
+        width: 196px !important;
+        height: 80px !important;
+        margin: 0 auto !important;
+        float: none !important;
+      }
+      .main, .main-content {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgJtczbU2Jv9qpwF4T6rqu9WmqenVXzRS-Od3aI7JvZI7R-L2bfiehNZ2uqqA6NbintnfBly2-GrMLuLDIwCQiwWr2g5fpzVlXbs551dnGY6qu9GyP51xq7g_oYbNUJQL1GuevzGtVh16vA5fy3gq4eqWeGIFwwCYJUnuXs5VOWXdAC9BWE4hr4K8yni0M/s1600/MOBILE-BACKGROUND.jpg) !important;
+        background-size: 100% !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
+
