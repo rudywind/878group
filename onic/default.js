6f32dbe033d69421ed9d80e80999d89bbd93eb61
rudywind <35211883+rudywind@users.noreply.github.com>
2025-08-27 18:20:07 +0700

    Create default.js

---
+++ onic/default.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      body {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgesSKHHoEIqk288zMNZWosA_o2GaCDWpbg_mwvI49035elFlb95J8C4gXeru_THkeRm4BsVXPK5CKgfKgzCR4z_Un4j5vycngL2v3kM4aWd4uDQ5MAU79MqQ9SmQT2vAmt9ihiFYQd7l-j80yMgzHH_asYlyicFDcaA2FSJmYdu417ctKqSyRyZnF04Qc/s1600/bg-onic.jpg) !important;
+        background-attachment: fixed !important;
+        background-size: 100% !important;
+      }
+      .navbar-fixed-top .container {
+        background-color: #00081300 !important;
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgJvRf24ThNvG0NSnhjA6LXxm5V3DiotUdEXYTnvfVtHYmougGV1zkHDxd3kN_APfV38iw_q_FmOr-w2LDn3Ony-U_kzdvdkOoNAvdi6d4fcSMIkIcfaLKywRB54hK05ldMhJa2G9KX7o72A_BOknrN2ry-Usn_NpPsM7YIegubq4bHt3rtSzFtbz10Fxg/s1600/Untitled-2.jpg);
+        background-size: 100%;
+      }
+      #content .page-header.smartb1 {
+        background: #000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj3TfuUF2YLLKJ9MBq_qst8HTTej-hwGNlUxCIbwH1wX7GO83g0hjo9yDAG6eKojheJH1AQenHhAVMOVkHHmpB8oR2fQVJ2_SuLEJVCF3KRnccFBbM62mrEvpo3_aEv-1-5iRxWI2i8SGGmLxRefOLsVgaewogOJ4QmJaK3tSrjbQamK0ksF0p1lkSb8nc/s1600/navbar.jpg);
+        background-size: 100%;
+      }
+      #content .buttonlc.space.smartb3 {
+        display: none;
+      }
+      .smartb2 .header-logo2 {
+        margin: 0 auto !important;
+        float: none !important;
+        margin-top: -45px !important;
+      }
+      a.smartb2 {
+        background: #000000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj3TfuUF2YLLKJ9MBq_qst8HTTej-hwGNlUxCIbwH1wX7GO83g0hjo9yDAG6eKojheJH1AQenHhAVMOVkHHmpB8oR2fQVJ2_SuLEJVCF3KRnccFBbM62mrEvpo3_aEv-1-5iRxWI2i8SGGmLxRefOLsVgaewogOJ4QmJaK3tSrjbQamK0ksF0p1lkSb8nc/s1600/navbar.jpg) !important;
+        background-position: center !important;
+        background-size: 100% !important;
+        box-shadow: 0 5px 10px #0000007d;
+        padding-top: 67px !important;
+      }
+      .page-header .buttonlc.space.smartb3 a.buttonlc2 {
+        display: none;
+      }
+      .main, .main-content {
+        background: #000000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgdNU_78TA9qlFbOzwmVODblKs56sxkge0WbfCV0jRmp5aBrJEwhyphenhyphenwiAnYhHPi-8WUx3HDZFM9UT7uG-5G02JRCYxvGfJ8ItWVfN5ZiUp7MaCG-waRtVlsoYIFD5-oDb-bOWPIF2uukK9c-GfC7Djzoyv0tsreaQE_17nEQhmqTWWu4nQ83Apd-NSilKyA/s1600/MOBILE-BACKGROUND%20%281%29.jpg) !important;
+        background-size: 100% !important;
+        background-attachment: fixed !important;
+        background-repeat: no-repeat !important;
+      }
+      div#header-wrap {
+        background: #000000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj3TfuUF2YLLKJ9MBq_qst8HTTej-hwGNlUxCIbwH1wX7GO83g0hjo9yDAG6eKojheJH1AQenHhAVMOVkHHmpB8oR2fQVJ2_SuLEJVCF3KRnccFBbM62mrEvpo3_aEv-1-5iRxWI2i8SGGmLxRefOLsVgaewogOJ4QmJaK3tSrjbQamK0ksF0p1lkSb8nc/s1600/navbar.jpg) !important;
+        background-position: center !important;
+        background-size: 100% !important;
+        box-shadow: 0 5px 10px #0000007d;
+        padding-top: 18px !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
