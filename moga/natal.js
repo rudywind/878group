00eb5f020ef9eefffd1ca643bcdf29d3a0778487
rudywind <35211883+rudywind@users.noreply.github.com>
2025-11-04 17:02:02 +0700

    Create natal.js

---
+++ moga/natal.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      a.smartb2 {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEihycHcbhkqzXX1NlSr-TaOmnOmb90rsYWUxK0NmKEnohVHYT4clyYSsbUJPCdM0EKcZmxi3VQk9tw3mWDzWHb7Xzpq86Mp7K86odV-wwtTnk3WwsxpLRargejHs8IqLcU4sQNXhXwmP7X_sNBc-W7pz4N46TDC7qTSXGcSEE4bli9OillgB40-EpVCQw/s1600/bg-navbar-moga.jpg) !important;
+          background-position: center !important;
+          background-size: cover !important;
+          box-shadow: 0 5px 10px #0000007d;
+      }
+      .smartb2 .header-logo2 {
+        width: 178px !important;
+        height: 70px !important;
+        margin: 0 auto !important;
+        float: none !important;
+      }
+      .main, .main-content {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgoJ7-l4eNxSNVoWSCL6dg8hz2VKYAq9YkJFNqSSQtZSl1hkZayFcTaoJr1SOja24TnTfuJxSUgSrjkaz9rMRcKjoh6kK-Ro6TDcDdAV_o8TM11DnJZdhrOTBYAANdEAfRmzsQjN36gz3p2CYE57lu8NAku8rz0vYO29ubMEdmuDTJ4_Vm-XtaJ-wUXXA/s1600/bg-mobile-moga.png) !important;
+          background-size: 100% !important;
+      }
+      .navbar-fixed-top {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgH3zSwiJUC5ZgztFNdxL1t5-yZ9vRFRILDOsoPHvyfeif4uvw2ZX-M4-9zb6sVS_YDNu0rDJJREBByXGpYMVOFvxnnQDdNx-fA3TWIIAujOee8azYny_OQWq7fs1IK_xicY4LVe8RYl5cH8TNSYP0e61o3zcYwO6ZbaFUpvgttxKy1pXxHdWWJo0ERug/s1600/navbar-moga.jpg);
+          background-size: cover;
+          background-repeat: no-repeat;
+          box-shadow: none !important;
+      }
+      body {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj65ZBYxwZK-u2GdcRtrGVWXpBHDfOiQ06GUCF5-i95Kgxue5IhqGef6Bm86YVnTpn12Oe9WyrMIJ3GpJYwMnj9SjhjatFdr8ojAYq-s60lTzk9PczesgVzRtXUTBHwQNL4zQyOcB8T5u8BzGfEeol49aal10dCTD63g_6vMBLc9wwAfXKjX_zQvabpEw/s1600/BG-NATAL-MOGA.png) !important;
+          background-attachment: fixed !important;
+          background-size: 100% !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
