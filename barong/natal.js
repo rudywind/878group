80a36661503d3a6b4b105b56d775ad7bfaa12599
rudywind <35211883+rudywind@users.noreply.github.com>
2025-11-04 15:21:38 +0700

    Add natal.js for festive theme customization

---
+++ barong/natal.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      body {
+      background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhXDsw_WWF1PGrQ9h1c1Em-GlRAu4Vod7kv5fVtOMoaOH-GgbB0R1Ckta3vVhNj8_jK-kEia1T_flRZaUj-o4MEOhGPTFYFiWzHmVdUX7kRtqyRMmPCHJtGClOGYMZlToftQXF0dqK4mqoEiuvSSTSJpSBEWd7tncNoYITGNZnkmsHRIb6Q_31gaBPpVQ/s1600/barong-bg-natal-tahun-baru.jpg) !important;
+      background-attachment: fixed !important;
+      background-size: 100% !important;
+  }
+      #content .page-header a.smartb2 {
+        background: #610000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjzAoqYltIkzKikUwDCexh3rAHIzsEWFCcr8x1gS02kCgYMZ1NoMXTu_10bYIyWy4Aek93yz58zhZ3P0gIDwwAOW4NpJkSGSl4-ZkNN0A29lBbLMsFOvKmbWd9sikzZbuUQVUG6r6URxgzXAOQtwUokZWrwd0nZsZk2Bny_v9If0ruRrLr8Uazny4xXjQ/s16000/navbra-mobile-barong.png) !important;
+        background-position: center !important;
+        background-size: cover !important;
+        box-shadow: 0 5px 10px #0000007d;
+    }
+    .smartb2 .header-logo2 {
+        width: 161px !important;
+        height: 55px !important;
+        margin: 0 auto !important;
+        float: none !important;
+        margin-top: 9px !important;
+        display: block;
+        padding-bottom: 9px;
+    }
+    .main, .main-content {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjKLUWHJnlxtL6aphixTBRZddeCB6bqLSNmEZ_qWf9x0rVeJULLgctcSDhbx2Xp6FABol-GyP6Z-iz8RCbTu_C0XbRhD6dcyc-s8DKBBj7tBCQp7NtZX2pxOmw21RG9OPLeuzdSOyCvu6fL1mMXz8slaJZXKZ9iWWscZh3CVcUP2hPJR8lvNz12wqNyww/s1600/mobile-background.jpg) !important;
+        background-size: 100% !important;
+    }
+    .navbar-fixed-top {
+        background-color: transparent !important;
+        box-shadow: none !important;
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhACPmUqqQayaPu-MA0zG_iNivC5dIZZtIWBrIj2riDw9SYStr4YeLvXDUXlnvwky25jCEmUzGpOrZbu7YntxHSz3v-fE0btw8ncL7HNnziAT8C5A2gACvLWS8PLoShxB6MVt8uY2kx9y9qkx0VnpW0LS48KvP-aId7ofskEcBTyXqHN9uyr9fxVR50xw/s1600/barong-navbar-natal.png) bottom center no-repeat;
+        background-size: cover;
+    }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+  document.head.insertAdjacentHTML("beforeend", `<script src="https://cdn.jsdelivr.net/gh/rudywind/878group@main/salju.js"><\/script>`);
+});
