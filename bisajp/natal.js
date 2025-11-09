f00719aec7c323602aa771917534becfaf672b00
rudywind <35211883+rudywind@users.noreply.github.com>
2025-11-09 10:42:44 +0700

    Create natal.js

---
+++ bisajp/natal.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDefault = `
+    <style>
+      body {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi7RtZpRtJ5VmIg59BACGD_M35Muuy_OLSW9NXUSgYCtHT1WCwVLh80Z0vvzG_eQ1oBjJPx6wMvBWfegOvsoBmjQ5E7BjmKBaLZrWSEz9trZJozUanKkxe9IciBvcJrT-ktbkqjaSQFMdj7zGwxPBhFcHH8T5WRftKgi7O_ndtlCTGmhX2qh7hzesGiBg/s1600/bg-jp-natal.png) !important;
+        background-attachment: fixed !important;
+        background-size: cover !important;
+      }
+      
+      .navbar-fixed-top {
+        background-color: transparent !important;
+        box-shadow: none !important;
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXun8vF2aOgT2Vv8maiC2gLN2GoSGN-MhHOsUHQM1urOHqZR4svIvM-WnB8hVhWGooCs73BC_huRq0DRnh0dlyUk47rkslxC-CmtogPJEKt1gkSrDejHfS1o5PpTkSgF3k_0yJ09BTogFFqOPhABHvu3w4J_hbXSPIesTH5VCqNB4jxuYDJv9jW4WeQA/s1600/NAVBAR-JP.jpg) bottom center no-repeat;
+        background-size: cover;
+      }
+      
+      .main-content, 
+      .content, 
+      .no-bottom{
+        background: url(hhttps://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhRenRQFobyg0b_oUmATZMkGa15J1e3lxWhpwgtTef_0dbfclt3RImaSMFv7C80dyGlIdAdroAQP2sG6oKuAx6WvDjrQbFNhxwIlnyu3TSRu8NMbQHpJNMUmnHx9QEl25gEKb313zClTD75AJJgrBstKho31GmavBew0BjvQh37KGRT28sQC0WnfBXQPg/s1600/Picsart_25-11-08_20-16-02-182.png) !important;
+        background-size: 100% !important;
+      }
+      
+      .smartb2 .header-logo2 {
+        width: 103px !important;
+        height: 59px !important;
+        margin: 0 auto !important;
+        float: none !important;
+      }
+      
+      a.smartb2 {
+        background: #000000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj3B2gjUQBt6ZrxBKZPWQ_JYhj5mPsnsWIDo9WMZAXzzpzqyF430jZ88ICkJ7Q2yvj6Q9V6kHvjExB1DxM_6pV1DfdrJL1MWg9vpM3mP9M5L_DGPUmZxU1YIEyX9ZrD2GFktb3Tp-B9jltVaf5D99C0RExvlRrdD4TezCcv24K8iZpgerfr1_81QJwvmw/s1600/bg-navbar-mobile.jpg) !important;
+        background-position: center !important;
+        background-size: 100% !important;
+        box-shadow: 0 5px 10px #0000007d;
+        padding-top: 10px;
+        padding-bottom: 10px;
+      }
+    </style>`;
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDefault);
+});
