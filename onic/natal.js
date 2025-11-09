55c920998802dadbbc9b9342180a9180068dc54e
rudywind <35211883+rudywind@users.noreply.github.com>
2025-11-09 21:01:08 +0700

    Create natal.js

---
+++ onic/natal.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      .navbar-fixed-top {
+        background-color: transparent !important;
+        box-shadow: none !important;
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi2yilvZ-5VLOUFbGHeqF5LvMvBz8Z5tysb8k6dxeLLEdGhNz3bhTxwm6j9KrVVwG6sUOT9BltyVFLGF5lr80DIAiMl0wt6nMxi1PjXv7qqKaJxX6dshHIzWiEASELKTcY3GQdmlHBCeU73iK_LyHL7sz5HCDVyY8JzeMAhbUJmh9weAW-59Iez183a3Q/s1600/NAVBAR-ONIC.jpg) bottom center no-repeat;
+        background-size: cover;
+      }
+      .navbar-fixed-top .container {
+          background: url() !important;
+      }
+      body {
+        background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgzVlK2uVHjW7gaAenQchnPvUp0h9SqbAy7AdA5dGKVWmmwsoTnERFv2LZ9DBWM6-cvTb9NHLEoqt1urYjsGgLixeIzpD4KHKn7q4NKeOURzoK9iAGg8bP94fMhHpLxl44V_xZmcaNREyc8akoAjFPZXQ4-TPTmCjhU8slaq14k8xtIfD0iBY13N5dQGA/s1600/Picsart_25-11-09_12-27-40-337.png) !important;
+        background-attachment: fixed !important;
+        background-size: 100% !important;
+      }
+      a.smartb2 {
+        background: #000000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjd6oq6IryBpEe90TPdhSNDYcYJ01v9cIusqPdJTFWqP1EGPtPAz2eUbEWsVaIZlS8F1K1Yc5BfugA02glCX5b8lcIl0cNbTcyWG8O0kOj87bs3rHrejbKdMAHYFx2i68hhtH8nS_NuDUj804g-uypH-qxL-CzfORTJsJYiCkt-WT-vdpayFqPlgWVfhg/s1600/238c5ab….jpg) !important;
+        background-position: center !important;
+        background-size: 100% !important;
+        box-shadow: 0 5px 10px #0000007d;
+        padding-top: 67px !important;
+      }
+      .main, .main-content {
+        background: #000000 url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjWllG5K1_BenIWKEWu1NaoEcmWrT5WeXSVhvanfNViQ5G5WQAtLjTdUYDVJtEbhlohcK8xffv-fhyphenhyphenHnxMu1kqwiCJ88XLXSlR9WixnuZGgJ2viQmRF83HQHnUh23pcXAONV0O-_1lu8dDb39u5zGVgLEqnYmXadc-EKahFBtv-62JdVi26CyLKORcAhw/s1600/Picsart_25-11-09_20-31-22-520.png) !important;
+        background-size: 100% !important;
+        background-attachment: fixed !important;
+        background-repeat: no-repeat !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
