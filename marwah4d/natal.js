a44bbd89d55ba71ff6370d838c555e354d5c7464
rudywind <35211883+rudywind@users.noreply.github.com>
2025-11-25 11:59:23 +0700

    Update natal.js

--- marwah4d/natal.js
+++ marwah4d/natal.js
@@ -1,17 +1,37 @@
-//KODE INI DIGUNAKAN UNTUK MENGUBAH BACKGROUND DEKSTOP DAN BACKGROUDNNAVBAR DEKSTOP UNTUK TEMA NATAL DAN TAHUN BARU
-//SELAIN MENGUBAH BACKGROUND DAN NAVBAR, KODE INI JUGA MENAMBAHKAN OVERLAY SALJU, DAN KARAKTER SANTA ZEUS DENGAN ANIMASI DI SEBELAH KIRI DAN KANAN
-
-
-
-
-document.head.insertAdjacentHTML("beforeend", `
-<style>body{ background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh9-SDflJo2fwhMa033nx5BzSmFzRld-O9TTpaJXO7mCYQ0DYyWrscnacCIUGL2z664_IcLA0JxrXhUPz9YNJkH4QFCJK8lQV2Ck0N9Agk-4Equp1-EvJ1fa_5WAXzwzuGvQ5xMnkgv-R7IfgPD_Lkxl05lSsVdfkeXhtnODGjJZ9_s_f6yUM59ZeslJlI/s1600/marwah4d-background-natal%20%281%29.jpg) !important; background-size: cover !important; background-repeat: no-repeat !important; background-attachment: fixed !important; background-position: center !important}
-.navbar, .navbar-fixed-top{ background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNrdfxmlo5th00OiOtOOL_vRmiKVUMCaGKH875bwOti2g-OWQwCnF-BdUoxNDg0cIT9UuWIxFfaP7zrX4pma4bY7Vy6ENY3QvzJTvMt2xpoGlSvSkglO6js_pHmDx49IlmdfI98KpYYfRd0ZkjDXm0OXIaoWL2jsxxXHHEXGraEidKbGtOZczw5cY8YPE/s1600/navbar-marwah-natal%20%282%29.png) !important; background-size: cover !important; background-repeat: no-repeat !important; background-attachment: fixed !important}
-</style>`);
-
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      a.smartb2 {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhrj1M7GQAFVAhDDzFFp3ApDFRUPReYbLFd2BFVB6_l1gkT__59EZUzyq3tA6XLYqjY-HbUadHIkG_rp-F1mhJrSshXpDEsdnLRNSdyHeYbtQ5spNMsU-avi5do12QmxOYlnhyGhLrHoW3j6Zc0kC6HydcFlR-BihMuTQ9bGOLRlRbvfYWG4mj4TK0OOg/s1600/bg-navbar-marwah.jpg) !important;
+          background-position: center !important;
+          background-size: cover !important;
+          box-shadow: 0 5px 10px #0000007d;
+      }
+      .smartb2 .header-logo2 {
+        width: 178px !important;
+        height: 70px !important;
+        margin: 0 auto !important;
+        float: none !important;
+      }
+      .main, .main-content {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiUh5muwXI3tDKRe293llcwgUMGd4DCKX6ALE6WCY8vB01sc2gL5lOxKmULKXOdc8FDCGfwvdqYjFTKDlfS_yA4zvzhgIolOSizyLB5kC1UQF_7_H-MKL5t-PVWoDjgNtT3a2MTsZarUxqsdoyW8k2fWq-p_3KFY75gfR1imcpyJLbkvacrjzQz6hx8XQ/s1600/bg.png) !important;
+          background-size: 100% !important;
+      }
+      .navbar-fixed-top {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNrdfxmlo5th00OiOtOOL_vRmiKVUMCaGKH875bwOti2g-OWQwCnF-BdUoxNDg0cIT9UuWIxFfaP7zrX4pma4bY7Vy6ENY3QvzJTvMt2xpoGlSvSkglO6js_pHmDx49IlmdfI98KpYYfRd0ZkjDXm0OXIaoWL2jsxxXHHEXGraEidKbGtOZczw5cY8YPE/s1600/navbar-marwah-natal%20%282%29.png);
+          background-size: cover;
+          background-repeat: no-repeat;
+          box-shadow: none !important;
+      }
+      body {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh9-SDflJo2fwhMa033nx5BzSmFzRld-O9TTpaJXO7mCYQ0DYyWrscnacCIUGL2z664_IcLA0JxrXhUPz9YNJkH4QFCJK8lQV2Ck0N9Agk-4Equp1-EvJ1fa_5WAXzwzuGvQ5xMnkgv-R7IfgPD_Lkxl05lSsVdfkeXhtnODGjJZ9_s_f6yUM59ZeslJlI/s1600/marwah4d-background-natal%20%281%29.jpg) !important;
+          background-attachment: fixed !important;
+          background-size: 100% !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+});
 const scriptElement = document.createElement('script');
 scriptElement.src = 'https://cdn.jsdelivr.net/gh/rudywind/878group@main/salju.js';
 document.head.appendChild(scriptElement);
-
- 
-
