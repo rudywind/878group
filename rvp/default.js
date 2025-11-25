fe4ae03e0a72b807e2c98d9225f6d3f60d187d42
rudywind <35211883+rudywind@users.noreply.github.com>
2025-11-25 14:25:22 +0700

    Create default.js

---
+++ rvp/default.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rdyyThemeDirgahayuMobile = `
+    <style>
+      .main, .main-content {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh5-q9z6mZmgi41uDfxfNYVVeC7osDL4tN16VSKzdfjS5ZF3FRCjNbhW_HSDqTSbBhl2WIpd5H55tgppzvLlySr-kCMtMClO7ssJQWtni8-75rZwgB4sgHyuZG_9qJGLhrLOOpvwwxCEcscHB8GYejLEv_WIq_bAGI3YBLChXvNlXb30wYQoGP-zSd5v5s/s1600/sdcfvbgnm.jpg) !important;
+          background-size: 100% !important;
+      }
+      body {
+          background: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgs2j_JT3fJLi9Iz6eOneiSKqL9w5jkdypc0LQgWsGMfB9_mMRStFLcEn-CZo6Mz-XFLbXABHHzjy3DCgOlsDLXSWinLCTl-PWvz5Av8i9R9Pc5b8c6hduHmMdFtUN_AlRn4kj4N943OWntluWmLr4SIZgXKcxEQlpSAABwmOlrWUj-KnKaXimhAizLdo4/s1600/bg.jpg) !important;
+          background-attachment: fixed !important;
+          background-size: 100% !important;
+      }
+    </style>`;
+  
+  document.body.insertAdjacentHTML('afterbegin', rdyyThemeDirgahayuMobile);
+}); 
