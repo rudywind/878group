e5028717d1c7adf67e88282a71257a852f360e19
rudywind <35211883+rudywind@users.noreply.github.com>
2025-11-28 19:57:35 +0700

    Update natal.js

--- bisajp/natal.js
+++ bisajp/natal.js
@@ -39,6 +39,3 @@ document.addEventListener("DOMContentLoaded", function () {
     </style>`;
   document.body.insertAdjacentHTML('afterbegin', rdyyThemeDefault);
 });
-const scriptElement = document.createElement('script');
-scriptElement.src = 'https://cdn.jsdelivr.net/gh/rudywind/878group@main/salju.js';
-document.head.appendChild(scriptElement);
